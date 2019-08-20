736a4adaa88f2e8a2cde4fe5971973252baaeb4f
Richard Jimenez <ricjim@gmail.com>
2019-08-20 18:11:21 -0300

    Add files via upload

--- Control de gastos/main.py
+++ Control de gastos/main.py
@@ -1,5 +1,8 @@
 # Modulo para introducir los gastos en una hoja de exel
 
+import datetime
+
+today = datetime.datetime.now()
 data = {}
 option = 0
 
@@ -9,15 +12,23 @@ def menuppl():
     print('2. Salir')
 
 def menu_carga_fac():
-    datalst = []
+    savelst = []
     print('{:-^20}'.format('Menu de carga'))
-    shop = input('Ingresa "M" para Carrefour\n"D" para Dia\n"C" para Coto\n"V" para verduleria\n"')
+    fecha = input('Ingresa la fecha de compra: (dd/mm/yy)\n#')
+    savelst.append(fecha)
+    tienda = input('Ingresa el local donde compraste:\n\tM) para Market/Carrefour\n\tD) para Dia\n\tC) para Coto\n\tV) para verduleria\n\tF) para fiambreria\n#')
+    savelst.append(tienda)
+    total = float(input('Ingresa el monto total gastado:\n#'))
+    savelst.append(total)
+    print('Compraste en {} el dia {}, por un monto de {}'.format(savelst[0], savelst[1], savelst[2]))
+    response = input('Deseas guardar la factura? (S/N)')
+    if response.lower() == 's':
+        data
 
 
-while option != 5:
+while option != 2:
     menuppl()
     option = int(input('Escoje la opcion deseada: '))
     if option == 1:
-        data = menu_carga_fac()
-
-menuppl()
+        inv_date, shop, amount = menu_carga_fac()
+        #data['agosto'] = menu_carga_fac()
